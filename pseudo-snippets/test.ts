035bd106d3caa73f23b7c18c77b7081c69c90813
danielzotti <daniel.zotti87@gmail.com>
2018-11-22 18:15:33 +0100

    Aggiunto edit e create model

--- pseudo-snippets/test.ts
+++ pseudo-snippets/test.ts
@@ -1,18 +1,100 @@
-// EDIT MODEL
-case ${1:Entity}ActionTypes.SetEditModel:
-const editingModel = action.payload.editModel;
-return {
-  ...state,
-  editModel: { ...editingModel }
-};
-case ${1:Entity}ActionTypes.UnsetEditModel:
-return { ...state, editModel: undefined };
-// CREATE MODEL
-case ${1:Entity}ActionTypes.SetCreateModel:
-const creatingModel = action.payload.createModel;
-return {
-  ...state,
-  editModel: { ...creatingModel }
-};
-case ${1:Entity}ActionTypes.UnsetCreateModel:
-return { ...state, createModel: undefined };
+import { Injectable } from "@angular/core";
+
+import { Observable, of } from "rxjs";
+
+import { Store } from "@ngrx/store";
+
+import { AppState } from "../store/state";
+import { IReportingPeriodEdit } from "src/app/components/reporting-period/edit/reporting-period-edit.models";
+import {
+  reportingPeriodCreateModel,
+  reportingPeriodCreateModelIsLoading,
+  reportingPeriodList,
+  reportingPeriodIsFetching,
+  reportingPeriodById,
+  reportingPeriodEditModel,
+  reportingPeriodEditModelIsLoading
+} from "../store/reporting-period/reporting-period.selectors";
+import {
+  ReportingPeriodCreate,
+  ReportingPeriodUpdate,
+  ReportingPeriodLoadCancel,
+  ReportingPeriodLoad,
+  ReportingPeriodDelete,
+  ReportingPeriodListLoad,
+  ReportingPeriodListLoadCancel
+} from "../store/reporting-period/reporting-period.actions";
+import { IReportingPeriodCreate } from "../components/reporting-period/create/reporting-period-create.models";
+import { IReportingPeriodDetail } from "../components/reporting-period/detail/reporting-period-detail.models";
+
+@Injectable()
+export class ReportingPeriodFacade {
+  // Fetch
+  isFetching$: Observable<boolean> = this.store.select(
+    reportingPeriodIsFetching
+  );
+
+  // Create
+  createModel$: Observable<Partial<IReportingPeriodCreate>> = this.store.select(
+    reportingPeriodCreateModel
+  );
+
+  // Update
+  editModel$: Observable<Partial<IReportingPeriodEdit>> = this.store.select(
+    reportingPeriodEditModel
+  );
+
+  // List
+  list$: Observable<Array<IReportingPeriodSelectListItem>> = this.store.select(
+    reportingPeriodList
+  );
+
+  constructor(private store: Store<AppState>) {}
+
+  // List
+  loadAll() {
+    this.store.dispatch(new ReportingPeriodListLoad());
+  }
+  cancelLoadAll() {
+    this.store.dispatch(new ReportingPeriodListLoadCancel());
+  }
+
+  // Create
+  create(reportingPeriod: IReportingPeriodCreate) {
+    this.store.dispatch(new ReportingPeriodCreate(reportingPeriod));
+  }
+  // Update
+  update(reportingPeriod: IReportingPeriodEdit) {
+    this.store.dispatch(new ReportingPeriodUpdate(reportingPeriod));
+  }
+  // Delete
+  delete(reportingPeriod: IReportingPeriodStoreEntity) {
+    this.store.dispatch(new ReportingPeriodDelete(reportingPeriod));
+  }
+
+  // Create Model
+  setReportingPeriodEditModel(reportingPeriod: IReportingPeriodEdit) {
+    this.store.dispatch(new ReportingPeriodSetEditModel(reportingPeriod));
+  }
+  unsetReportingPeriodEditModel() {
+    this.store.dispatch(new ReportingPeriodUnsetEditModel());
+  }
+  // Edit Model
+  setReportingPeriodCreateModel(reportingPeriod: IReportingPeriodCreate) {
+    this.store.dispatch(new ReportingPeriodSetCreateModel(reportingPeriod));
+  }
+  unsetReportingPeriodCreateModel() {
+    this.store.dispatch(new ReportingPeriodUnsetCreateModel());
+  }
+
+  // Get By Id
+  loadById(reportingPeriodId: number) {
+    this.store.dispatch(new ReportingPeriodLoad(reportingPeriodId));
+  }
+  cancelLoadById() {
+    this.store.dispatch(new ReportingPeriodLoadCancel());
+  }
+  getById(reportingPeriodId: number) {
+    return this.store.select(reportingPeriodById(reportingPeriodId));
+  }
+}
